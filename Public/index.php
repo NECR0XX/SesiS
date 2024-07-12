83d3ac10ccaebcf9b44befddf08e3c413be8d277
NECR0XX <carronananiasx@gmail.com>
2024-07-11 23:44:10 -0300

    Início - Pastas, Arquivos, organização

---
+++ Public/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Resources/Css/index.css">
+    <title>Site Foda</title>
+</head>
+<body>
+    <h1>Site Foda</h1>
+    <div class="menu_section">
+        <nav>
+            <a href="disciplinas/disciplina.php" class="dropbtn" ><span>Disciplinas</span></a>
+            <div class="menu_drop">
+                <ul>
+                    <li><a href="disciplinas/mat.php">Matemática</a></li>
+                    <li><a href="disciplinas/port.php">Português</a></li>
+                    <li><a href="disciplinas/ingles.php">Inglês</a></li>
+                    <li><a href="disciplinas/quimica.php">Química</a></li>
+                    <li><a href="disciplinas/fisica.php">Física</a></li>
+                    <li><a href="disciplinas/bio.php">Biologia</a></li>
+                    <li><a href="disciplinas/geo.php">Geografia</a></li>
+                    <li><a href="disciplinas/hist.php">História</a></li>
+                </ul>
+            </div>
+        </nav>
+        <nav>
+            <a href="/exercicios/exercicios.php" class="dropbtn" ><span>Exercícios</span></a>
+            <div class="menu_drop">
+                <ul>
+                    <li><a href="#">Exercícios de Matemática</a></li>
+                    <li><a href="#">Exercícios de Português</a></li>
+                    <li><a href="#">Exercícios de Inglês</a></li>
+                    <li><a href="#">Exercícios de Química</a></li>
+                    <li><a href="#">Exercícios de Física</a></li>
+                    <li><a href="#">Exercícios de Biologia</a></li>
+                    <li><a href="#">Exercícios de Geografia</a></li>
+                    <li><a href="#">Exercícios de História</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</body>
+</html>
